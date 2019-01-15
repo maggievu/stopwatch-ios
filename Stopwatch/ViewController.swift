15ec2a7632398c10b74e3e6401cbf49ac225f563
Maggie VU <maggievu@maggies-macbook-pro.local>
2019-01-14 17:15:45 -0800

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Maggie VU on 2019-01-14.
+//  Copyright © 2019 Maggie VU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
